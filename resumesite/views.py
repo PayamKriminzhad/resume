89f746d750e52869fee800d65da15b83029badeb
Payam <payamkriminzhad@gmail.com>
2021-12-07 16:19:09 +0330

    saat

--- resumesite/views.py
+++ resumesite/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import post , profile, skills
+import requests
+import json
+
 
 def home(request):
     profile_1 = profile.objects.first()
@@ -10,9 +13,12 @@ def home(request):
     if request.method == "POST":
         post.objects.create(name = request.POST.get('name') ,email=request.POST.get('_replyto') ,subject=request.POST.get('subject') ,message=request.POST.get('message') , )
 
+    response = requests.get('http://worldtimeapi.org/api/timezone/Europe/Amsterdam')
+    r = response.json()["datetime"]
 
     context = {
         'profile' : profile_1,
         'skills' : skills_1,
+        'time' : r[11:19],
     }
     return render(request , 'index.html' , context)
